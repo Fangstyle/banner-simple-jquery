2c38def3d73fdbf356a5a9cdae733d36db831f9d
Fangstyle <FangStyles@163.com>
2017-03-19 19:37:47 +0800

    轮播效果完成

--- js/index.js
+++ js/index.js
@@ -1,3 +1,28 @@
 /**
  * Created by Administrator on 2017/3/19.
  */
+$(function () {
+    var page = 0;
+    var perAnimater = 750;
+    var currentLeft=0;
+    console.log($('.banner-contianer').offset().left);
+    $("#pointer a:first-child").on('click',function (event) {
+        if(page>0){
+            page--;
+           /* var currentLeft = $('.banner-contianer').offset().left;*/
+            currentLeft+=perAnimater;
+            $('.banner-contianer').animate({left:currentLeft},'slow');
+            console.log(page +" :"+ currentLeft);
+        }
+
+    });
+    $("#pointer a:last-child").on('click',function (event) {
+       if(page<4){
+        /*   var currentLeft = $('.banner-contianer').offset().left;*/
+           currentLeft-=perAnimater;
+           $('.banner-contianer').animate({left:currentLeft},'slow');
+           page++;
+           console.log(page +" :"+ currentLeft);
+       }
+    });
+});
